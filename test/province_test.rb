182430442569eed6c56dae230e11c9c30906abb5
cooperphyz <gjcooper99@gmail.com>
2022-05-13 13:47:09 -0700

    Add test for changing production

--- test/province_test.rb
+++ test/province_test.rb
@@ -11,8 +11,12 @@ class ProvinceTest < Minitest::Test
   def test_province_shortfall
     assert_equal(5, @asia.shortfall)
   end
-
   def test_province_profit
     assert_equal(230, @asia.profit)
+  end
+  def test_province_change_broduction
+    @asia.producers[0].production = 20;
+    assert_equal(-6, @asia.shortfall)
+    assert_equal(292, @asia.profit)
   end
 end
